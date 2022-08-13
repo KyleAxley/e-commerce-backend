bd2ecd77f95519e63f9343b2c60b0c131240ad5b
KyleAxley <kanderson7645@yahoo.com>
2022-08-13 18:50:43 -0400

    category route updated

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: ''
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.benlongToMany(Tag, {
+  foreignKey: ''
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreingkey: ''
+})
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,27 +2,87 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+//CRUD *** create(POST) receive(GET) update(PUT) delete(DELETE) aka "pogepude"...it's silly to put this but this helps me to remember...
 
+//api route to find all in categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll()
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    res.status(500).json(err)
+  });
 });
 
+//api route to find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    //if no category is found by "id" input search then send error message
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(400).json({ message: 'No category found with that id!'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+//api route to create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
+//api route to update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.body.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category was found with this id'});
+      return;
+    }
+    res.json(dbCategoryData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+//api route to delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category was found with this id'});
+      return;
+    }
+    res.json(dbCategoryData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
